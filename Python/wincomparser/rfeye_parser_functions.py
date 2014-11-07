2440c283adb22ad3eec997486ecbcb7901a55b8c
Eric Faurie <efaurie@hawk.iit.edu>
2014-11-06 22:36:18 -0600

    Grr Numpy FromFile Would Have Been Great
    Was using Numpy FromFile, it works great, but RfEye has a lot of dynamic
    blocks that have variably sized fields. It's not going to work. Going to
    have to refactor.

--- Python/wincomparser/rfeye_parser_functions.py
+++ Python/wincomparser/rfeye_parser_functions.py
@@ -3,7 +3,21 @@ import numpy as np
 from Band import Band
 
 EOF = int('0x55555555', 0)
+RFEYE_DATE = np.dtype([('day', np.uint8), ('month', np.uint8), ('year', np.uint8), ('pad', np.uint8)])
+RFEYE_TIME = np.dtype([('hours', np.uint8), ('minutes', np.uint8), ('seconds', np.uint8), ('centiseconds', np.uint8)])
 
+DATA_TYPES = dict({
+    'file_header': np.dtype([('version', np.int32), ('description', np.str_, 32)]),
+    'block_header': np.dtype([('data_thread', np.int32), ('bytes_in_block', np.int32), ('data_type', np.int32)]),
+    'block_trailer': np.dtype([('checksum', np.int32), ('eof_marker', np.int32)]),
+    'data_type': dict({
+        1: np.dtype([('place_holder', np.int32)]),
+        40: np.dtype([('clock_date', RFEYE_DATE), ('clock_time', RFEYE_TIME), ('clock_nano', np.uint32),
+                      ('gps_date', RFEYE_DATE), ('gps_time', RFEYE_TIME), ('position_fix', np.uint8),
+                      ('satellites', np.uint8), ('heading', np.int16), ('latitude', np.int32), ('longitude', np.int32),
+                      ('speed', np.int32), ('altitude', np.int32)]),
+    }),
+})
 
 def open_rfeye_file(filepath):
     data_file = file(filepath, 'rb')
@@ -19,8 +33,7 @@ def parse_rfeye_file(data_file):
 
 
 def _parse_file_header(data_file):
-    header = np.dtype([('version', np.int32), ('description', np.str_, 32)])
-    data = np.fromfile(data_file, dtype=header)[0]
+    data = np.fromfile(data_file, dtype=DATA_TYPES['header'], count=1)[0]
     return dict({
         'version': int(data[0]),
         'description': data[1]
@@ -29,28 +42,27 @@ def _parse_file_header(data_file):
 
 def _parse_data(data_file):
     data = []
-
     while True:
         data_block = dict()
-        data_block['header'] = _parse_block_header(data_file)
 
-        bytes_in_block = data_block['header']['bytes_in_block']
-        data_type = data_block['header']['data_type']
+        try:
+            data_block['header'] = _parse_block_header(data_file)
 
-        data_block['payload'] = _parse_block_payload(data_file, bytes_in_block, data_type)
-        data_block['trailer'] = _parse_block_trailer(data_file)
+            bytes_in_block = data_block['header']['bytes_in_block']
+            data_type = data_block['header']['data_type']
 
-        data.append(data_block)
+            data_block['payload'] = _parse_block_payload(data_file, bytes_in_block, data_type)
+            data_block['trailer'] = _parse_block_trailer(data_file)
 
-        if data_block['trailer']['eof_marker'] == EOF:
+            data.append(data_block)
+        except EOFError:
             break
 
     return data
 
 
 def _parse_block_header(data_file):
-    block_header = np.dtype([('data_thread', np.int32), ('bytes_in_block', np.int32), ('data_type', np.int32)])
-    data = np.fromfile(data_file, dtype=block_header)[0]
+    data = np.fromfile(data_file, dtype=DATA_TYPES['block_header'], count=1)[0]
     return dict({
         'data_thread': int(data[0]),
         'bytes_in_block': int(data[1]),
@@ -59,8 +71,7 @@ def _parse_block_header(data_file):
 
 
 def _parse_block_trailer(data_file):
-    block_trailer = np.dtype([('checksum', np.int32), ('eof_marker', np.int32)])
-    data = np.fromfile(data_file, dtype=block_trailer)[0]
+    data = np.fromfile(data_file, dtype=DATA_TYPES['block_trailer'], count=1)[0]
     return dict({
         'checksum': int(data[0]),
         'eof_marker': int(data[1]),
@@ -69,7 +80,7 @@ def _parse_block_trailer(data_file):
 
 def _parse_block_payload(data_file, bytes_in_block, data_type):
     block_payload = np.dtype([('throw_away', np.str_, bytes_in_block)])
-    data = np.fromfile(data_file, dtype=block_payload)[0]
+    data = np.fromfile(data_file, dtype=block_payload, count=1)[0]
     return dict({
         'throw_away': data[0],
     })
